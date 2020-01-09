2333951056407bb8d693a8ef66bfaa88afac8104
Matt Wright <mttwright@gmail.com>
2020-01-09 01:02:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,37 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+  
+  def self.column_names
+    DB[:conn].results_as_hash = true
+  
+    sql = <<-SQL
+      pragma table_info('#{table_name}')
+    SQL
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |row|
+      column_names << row["name"]
+    end
+    column_names.compact
+  end
+  
+  def initialize(properties={})
+    properties.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+  end
+  
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_names_for_insert
+    self.class.column_names
+  end
+  
 end
